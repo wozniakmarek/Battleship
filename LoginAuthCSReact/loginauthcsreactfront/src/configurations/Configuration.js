ce77ed028e9997c7cfd46db7b042f5eef65c288d
Marek Woźniak <marek.wozniak@microsoft.wsei.edu.pl>
2023-01-31 01:07:41 +0100

    Revert "Results"
    This reverts commit 6126dcba5f760c1a683a16cce457064c29297aa2.

--- LoginAuthCSReact/loginauthcsreactfront/src/configurations/Configuration.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    SignUp: 'https://localhost:44380/api/Auth/SignUp',
-    SignIn: 'https://localhost:44380/api/Auth/SignIn',
-    CreateDummyUser: "https://localhost:44380/api/Auth/CreateDummyUser",
-  }
--- LoginAuthCSReact/loginauthcsreactfront/src/services/AxiosServices.js
+++
@@ -1,9 +0,0 @@
-
-
-const axios = require('axios')
-
-export default class AxiosServices {
-  post(url, data, isRequiredHeader = false, header) {
-    return axios.post(url, data, isRequiredHeader && header)
-  }
-}
